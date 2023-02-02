41e3c9f0105403807fb3c0d75a4bd116571b2887
Gabriel Rocha <rochagbriel@gmail.com>
2023-02-02 12:22:58 +0100

    Code Structur done

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'Simple Vue.js ToDo List'
+        title: 'Simple Vue.js ToDo List',
+        newTodo: '',
+        todos: []
+    },
+    methods: {
+        addTodo() {                                           //add a new todo
+            this.todos.push({           
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo = '';                                //make the textbox empty after submit
+        },
+        removeTodo(todo) {                                    //delete todo
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        },
+        allDone() {                                           //mark all as 'done'
+            this.todos.forEach(todo => {
+                todo.done = true;
+            });
+        }
     }
 });
